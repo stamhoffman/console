623052b252904e6b6d7fa778beafeff5927426e8
stamhoffman <stamhoffman@yandex.ru>
2017-07-12 21:22:49 +0300

    На сервере забиндил  SOCK_STREAM, подключился клиентом, на сервере вывел сообщение с каким адресом установлено соединение

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,10 +1,72 @@
-
-#include <iostream>
-#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(int argc,char **argv)
+
+
+#define pointer_adrr const struct sockaddr
+#define print_info
+
+int main(int argc, char **argv)
 {
-  std::cout << "/* message client*/" << '\n';
-  return 0;
+
+	int socket_descriptor;
+
+	if((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Error socket():%s\n", strerror(errno));
+		return -1;
+	}
+		else
+		{
+			#ifdef print_info
+			printf("socket_descriptor OK\n");
+			#endif
+		}
+
+
+	struct sockaddr_in servaddr_options;
+
+	memset(&servaddr_options, 0 ,sizeof(servaddr_options));
+
+	servaddr_options. sin_family = AF_INET;
+	servaddr_options. sin_port = htons(54321);
+
+	if(inet_pton(AF_INET, argv[1], &servaddr_options.sin_addr) <= 0)
+	{
+		printf("Error inet_pton():%s\n", strerror(errno));
+		return -1;
+	}
+		else
+		{
+			#ifdef print_info
+			printf("inet_pton OK\n");
+			#endif
+		}
+
+	connect(socket_descriptor, (pointer_adrr *)&servaddr_options, sizeof(servaddr_options));
+
+	#define MAXLINE 50
+	char buffer[MAXLINE + 1];
+
+	int n;
+
+	while((n = read(socket_descriptor, buffer, MAXLINE)) > 0)
+	{
+
+		buffer[n] = 0;
+
+		if(fputs(buffer, stdout) == EOF)
+		{
+			printf("END SEND%s\n",strerror(errno));
+		}
+	}
+
+	close(socket_descriptor);
+
+	return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,8 +1,72 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
+
+
+#define pointer_socket struct sockaddr
 
 int main(int argc, char **argv)
 {
-  std::cout << "/* message server*/" << '\n';
-  return 0;
+	int       socket_descriptor, socket_accept;
+	socklen_t len;
+
+	if((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Error socket():%s\n", strerror(errno));
+		return -1;
+	}
+
+	struct sockaddr_in address_clienta;
+	struct sockaddr_in address_accept;
+	memset(&address_clienta, 0, sizeof(address_clienta));
+	memset(&address_accept, 0, sizeof(address_accept));
+
+	address_clienta.sin_family = AF_INET;
+	address_clienta.sin_port = htons(54321);
+	address_clienta.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if((bind(socket_descriptor, (pointer_socket *)&address_clienta,
+  sizeof(address_clienta))) == -1)
+	{
+		printf("Error bind():%s\n", strerror(errno));
+		return -1;
+	}
+
+	if((listen(socket_descriptor, 3)) == -1)
+	{
+		printf("Error listen()%s\n",strerror(errno));
+	}
+
+	len = sizeof(socket_accept);
+	char addr_clients[INET_ADDRSTRLEN];
+	const char *res;
+
+
+	while(1)
+	{
+		len = sizeof(socket_accept);
+
+		if((socket_accept = accept(socket_descriptor, (pointer_socket *)
+    &address_accept, &len)) == -1)
+		{
+			printf("Errorr accept():%s\n",strerror(errno));
+		}
+
+		if((res = inet_ntop(AF_INET, (pointer_socket *)&address_accept,
+    addr_clients, sizeof(addr_clients))) == NULL)
+		{
+			printf("Error inet_ntop():%s\n",strerror(errno));
+		}
+    else printf("connection from: %s\n", addr_clients);
+
+	}
+
+	close(socket_descriptor);
+	close(socket_accept);
+	return 0;
 }
