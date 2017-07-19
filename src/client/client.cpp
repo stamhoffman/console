33767f7b1d6277799594640fded0c136104839ec
stamhoffman <stamhoffman@yandex.ru>
2017-07-19 12:28:00 +0300

    Исправления замечаний по оформлению кода в client.cpp

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -10,63 +10,63 @@
 
 int main(int argc, char **argv)
 {
-	#define pointer_adrr const struct sockaddr
-	#define PORT 54321
+  #define pointer_adrr const struct sockaddr
+  #define PORT 54321
 
-	if(argc < 2)
-	{
-		std::cout << "Ошибка в формате команды, не введен адрес сервера\n"<< std::endl;
-		std::cout << "Используйте ключ -h для получения справки \n" << std::endl;
-		return -1;
-	}
+  if(argc < 2)
+  {
+    std::cout << "Ошибка в формате команды, не введен адрес сервера\n"<< std::endl;
+    std::cout << "Используйте ключ -h для получения справки \n" << std::endl;
+    return -1;
+  }
 
-	const char helpstr[3] = "-h";
+  const char helpstr[3] = "-h";
 
   if(strncmp(argv[1], helpstr, 2) == 0)
-	{
-		std::cout<<"Формат команды "<<argv[0]<<" 127.1.1.1"<<std::endl;
-		return 0;
-	}
+  {
+    std::cout<<"Формат команды "<<argv[0]<<" 127.1.1.1"<<std::endl;
+    return 0;
+  }
 
-	int socket_descriptor;
+  int socket_descriptor;
 
-	if((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		std::cout << "Error socket():" <<strerror(errno) << std::endl;
-		return -1;
-	}
+  if((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    std::cout << "Error socket():" <<strerror(errno) << std::endl;
+    return -1;
+  }
 
-	struct sockaddr_in serv_addr;
-	memset(&serv_addr, 0 ,sizeof(serv_addr));
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0 ,sizeof(serv_addr));
+  serv_addr. sin_family = AF_INET;
+  serv_addr. sin_port = htons(PORT);
 
-	serv_addr. sin_family = AF_INET;
-	serv_addr. sin_port = htons(PORT);
+  if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) == -1)
+  {
+    std::cout << "Error inet_pton():"<< strerror(errno) << std::endl;
+    return -1;
+  }
 
-	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) == -1)
-	{
-		std::cout << "Error inet_pton():"<< strerror(errno) << std::endl;
-		return -1;
-	}
+  if(connect(socket_descriptor, (pointer_adrr *)&serv_addr, sizeof(serv_addr)) == -1)
+  {
+    std::cout << "Error connect:" << strerror(errno) << std::endl;
+  }
 
-	if(connect(socket_descriptor, (pointer_adrr *)&serv_addr, sizeof(serv_addr)) == -1)
-	{
-		std::cout << "Error connect:" << strerror(errno) << std::endl;
-	}
+  #define MAXLINE 50
+  char buffer[MAXLINE];
 
-	#define MAXLINE 50
-	char buffer[MAXLINE];
+  while(1)
+  {
+    scanf("%s",buffer);
 
-	while(1)
-	{
-		scanf("%s",buffer);
+    if(strcmp(buffer,"exit") == 1)
+    {
+      close(socket_descriptor);
+      break;
+    }
 
-		if(strcmp(buffer,"exit") == 1)
-		{
-			close(socket_descriptor);
-			break;
-		}
-		send(socket_descriptor,buffer, sizeof(buffer),0);
-	}
+  send(socket_descriptor,buffer, sizeof(buffer),0);
+  }
 
-	return 0;
+return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -15,59 +15,60 @@ int main(int argc, char **argv)
   #define pointer_socket struct sockaddr
   #define PORT 54321
 
-  int socket_descriptor, socket_accept;
+  int listening_socket, client_socket;
 
-  if((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  if((listening_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
     printf("Error socket():%s\n", strerror(errno));
     return -1;
   }
 
-  struct sockaddr_in address_clienta;
-  struct sockaddr_in address_accept;
-  memset(&address_clienta, 0, sizeof(address_clienta));
-  memset(&address_accept, 0, sizeof(address_accept));
+  struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+  struct sockaddr_in client_address;
+  memset(&client_address, 0, sizeof(client_address));
 
-  address_clienta.sin_family = AF_INET;
-  address_clienta.sin_port = htons(PORT);
-  address_clienta.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(PORT);
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if((bind(socket_descriptor, (pointer_socket *)&address_clienta, sizeof(address_clienta))) == -1)
+  if((bind(listening_socket, (pointer_socket *)&server_address, sizeof(server_address))) == -1)
   {
     printf("Error bind():%s\n", strerror(errno));
     return -1;
   }
 
 
-  if((listen(socket_descriptor, 3)) == -1)
+  if((listen(listening_socket, 3)) == -1)
   {
     printf("Error listen()%s\n", strerror(errno));
   }
 
   while(1)
   {
-    socklen_t len = sizeof(address_accept);
+    socklen_t length = sizeof(client_address);
 
-    if((socket_accept = accept(socket_descriptor, (pointer_socket *)&address_accept, &len)) == -1)
+    if((client_socket = accept(listening_socket, (pointer_socket *)&client_address, &length)) == -1)
     {
-      printf("Errorr accept():%s\n", strerror(errno));
+      printf("Errorr accept():%s\n",strerror(errno));
     }
 
   char addr_clients[INET_ADDRSTRLEN];
-  inet_ntop(AF_INET, &(address_accept.sin_addr), addr_clients, INET_ADDRSTRLEN);
+
+  inet_ntop(AF_INET, &(client_address.sin_addr), addr_clients, INET_ADDRSTRLEN);
   printf("connection from: %s\n", addr_clients);
 
-  std::thread thr(client_connect, std::ref(socket_accept));
+  std::thread thr(client_connect, std::ref(client_socket));
   thr.detach();
   }
 
-  close(socket_descriptor);
-  close(socket_accept);
+  close(listening_socket);
+  close(client_socket);
 
 return 0;
 }
 
-int client_connect(int socket_accept)
+int client_connect(int client_socket)
 {
   std::cout << "/* client_connect start */" << '\n';
 
@@ -77,18 +78,18 @@ int client_connect(int socket_accept)
 
   while(1)
   {
-    while((n = read(socket_accept, buffer, MAXLINE)) > 0)
+    while((n = read(client_socket, buffer, MAXLINE)) > 0)
     {
       std::cout << buffer << std::endl;
 
     if(strncmp(buffer, "ls", 2) == 1)
     {
-      write(socket_accept, "buffer", sizeof("buffer"));
+      write(client_socket, "buffer", sizeof("buffer"));
     }
 
-    if(strncmp(buffer,"exit",4) == 1)
+    if(strncmp(buffer, "exit", 4) == 1)
     {
-      close(socket_accept);
+      close(client_socket);
       return 0;
     }
    }
