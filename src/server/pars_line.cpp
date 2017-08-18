b4cd260237550ae717cf6b4ecdec2fa2ebf24595
stamhoffman <stamhoffman@yandex.ru>
2017-08-18 06:44:16 +0300

    Рефакторинг функции pars_line. Для поиска пробельных символово использован std::isgraph, для итерации по значащим символам значения возвращаемые std::isalnum

--- src/server/pars_line.cpp
+++ src/server/pars_line.cpp
@@ -8,92 +8,61 @@ int pars_line(std::array<char, 1000> *read_buff,
     return -1;
   }
 
-  std::array<char, 1000> symbol = {{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}};
-  std::array<char, 1000> end_symbol = {{' ', '\t', '\0'}};
-
-  *prog_name = {};
-  *prog_key = {};
-
   std::array<char, 1000>::iterator start_itr = read_buff->begin();
   std::array<char, 1000>::iterator end_itr = read_buff->begin();
   std::array<char, 1000>::iterator pn_itr = prog_name->begin();
   std::array<char, 1000>::iterator key_itr = prog_key->begin();
+  int count = 0;
 
-  if (read_buff->empty()) {
-    *prog_name = {};
-    *prog_key = {};
+  if(*start_itr == '\0')
+  {
+    *pn_itr = '\0';
+    *key_itr = '\0';
     return -1;
   }
 
-  start_itr = std::find(end_itr, read_buff->end(), '\0');
-
-  if (start_itr == (read_buff->begin())) {
-    *prog_name = {};
-    *prog_key = {};
+  while(std::isgraph(*start_itr) == 0){
+    start_itr++;
+  }
+  if (start_itr == (read_buff->end())) {
     return -1;
   }
 
-  start_itr = std::find_first_of(read_buff->begin(), read_buff->end(),
-                                 symbol.begin(), symbol.end());
-  if (start_itr == read_buff->end()) {
-    *prog_name = {};
-    *prog_key = {};
-    return -1;
-  }
+  end_itr = start_itr;
 
-  end_itr = std::find_first_of(start_itr, read_buff->end(), end_symbol.begin(),
-                               end_symbol.end());
-
-  if (end_itr == (read_buff->end())) {
-    *prog_name = {};
-    *prog_key = {};
-    return -1;
-  } else {
-    std::copy(start_itr, end_itr, pn_itr);
-  }
-
-  if ((*end_itr) == '\0') {
-    *prog_key = {};
-    int count;
-    for (count = 0; (*prog_name)[count] != '\0'; count++)
-      ;
-    if (count > 25) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }
-
-  while((*end_itr == ' ') || (*end_itr == '\t')){
+  while (std::isalnum(*end_itr) != 0) {
     end_itr++;
+    count++;
+    std::cout << "end_itr:" << end_itr[count] << '\n';
   }
 
   if (end_itr == (read_buff->end())) {
-    *prog_key = {};
     return -1;
   }
 
-  if (((*end_itr) == '\0')) {
-    *prog_key = {};
+  if(*end_itr == '\0'){
+    std::copy(start_itr, end_itr, pn_itr);
     return 0;
+  }
+  else{
+    pn_itr[count] = '\0';
   }
 
   start_itr = end_itr;
 
-  end_itr = std::find_first_of(start_itr, read_buff->end(), end_symbol.begin(), end_symbol.end());
-  {
-    if(end_itr == read_buff->end()){
-      return -1;
-    }
+  while(std::isgraph(*start_itr) == 0){
+    start_itr++;
+  }
+  if (start_itr == (read_buff->end())) {
+    return -1;
+  }
+
+  end_itr = std::find(start_itr, read_buff->end(), '\0');
+  if (end_itr == (read_buff->end())) {
+    return -1;
+  }
+  else{
     std::copy(start_itr, end_itr, key_itr);
-    int count;
-    for (count = 0; (*prog_name)[count] != '\0'; count++)
-      ;
-    if (count > 25) {
-      return -1;
-    }
     return 0;
   }
 }
