02e63a88fc0316d575130d512d8070f28a10f97a
stamhoffman <stamhoffman@yandex.ru>
2017-08-21 03:40:44 +0300

    Рефакторинг функции pars_line

--- src/server/pars_line.cpp
+++ src/server/pars_line.cpp
@@ -12,48 +12,10 @@ int pars_line(std::array<char, 1000> *read_buff,
   std::array<char, 1000>::iterator end_itr = read_buff->begin();
   std::array<char, 1000>::iterator pn_itr = prog_name->begin();
   std::array<char, 1000>::iterator key_itr = prog_key->begin();
-  int count = 0;
 
-  if(*start_itr == '\0')
-  {
-    *pn_itr = '\0';
-    *key_itr = '\0';
-    return -1;
-  }
-
-  while(std::isgraph(*start_itr) == 0){
-    start_itr++;
-  }
-  if (start_itr == (read_buff->end())) {
-    return -1;
-  }
-
-  end_itr = start_itr;
-
-  while (std::isalnum(*end_itr) != 0) {
-    end_itr++;
-    count++;
-    std::cout << "end_itr:" << end_itr[count] << '\n';
-  }
-
-  if (end_itr == (read_buff->end())) {
-    return -1;
-  }
-
-  if(*end_itr == '\0'){
-    std::copy(start_itr, end_itr, pn_itr);
-    return 0;
-  }
-  else{
-    pn_itr[count] = '\0';
-  }
-
-  start_itr = end_itr;
-
-  while(std::isgraph(*start_itr) == 0){
-    start_itr++;
-  }
-  if (start_itr == (read_buff->end())) {
+  if (*start_itr == '\0') {
+    prog_key -> fill('\0');
+    prog_name -> fill('\0');
     return -1;
   }
 
@@ -61,8 +23,56 @@ int pars_line(std::array<char, 1000> *read_buff,
   if (end_itr == (read_buff->end())) {
     return -1;
   }
-  else{
-    std::copy(start_itr, end_itr, key_itr);
+
+  while (std::isgraph(*start_itr) == 0) {
+    start_itr++;
+  }
+  if (start_itr == (read_buff->end())) {
+    return -1;
+  } else {
+    end_itr = start_itr;
+  }
+
+  while (std::isgraph(*end_itr) != 0) {
+    end_itr++;
+  }
+
+  if (end_itr == (read_buff->end())) {
+  return -1;
+  }
+
+  if (*end_itr == '\0') {
+    pn_itr = std::copy(start_itr, end_itr , pn_itr);
+    *pn_itr = '\0';
+    prog_key -> fill('\0');
+    return 0;
+  } else {
+    pn_itr = std::copy(start_itr, end_itr , pn_itr);
+    *pn_itr = '\0';
+  }
+
+  start_itr = end_itr;
+
+  while (std::isgraph(*start_itr) == 0) {
+    start_itr++;
+  }
+  if (start_itr == (read_buff->end())) {
+  return -1;
+  }
+
+  end_itr  = start_itr;
+
+  while (std::isgraph(*end_itr) != 0) {
+    end_itr++;
+  }
+
+  if (*end_itr == '\0') {
+    key_itr = std::copy(start_itr, end_itr , key_itr);
+    *key_itr = '\0';
+    return 0;
+  } else {
+    key_itr = std::copy(start_itr, end_itr, key_itr);
+    *key_itr = '\0';
     return 0;
   }
 }
