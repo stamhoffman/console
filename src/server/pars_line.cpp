94a162c452fe5071b9b576d97ac3b4588798f11e
stamhoffman <stamhoffman@yandex.ru>
2017-09-21 23:24:49 +0300

    Исправлена функция pars_line: добавляется nullptr в конец возвращаемого вектора. Переписаны тесты:добавлена проверка числа указателей и растановка символа конца строки во входном массиве

--- src/server/pars_line.cpp
+++ src/server/pars_line.cpp
@@ -6,25 +6,27 @@ std::vector<char *> pars_line(std::array<char, 1000> &user_string) {
   std::array<char, 1000>::iterator start_itr = user_string.begin();
   std::array<char, 1000>::iterator end_itr = user_string.begin();
 
-  if(*start_itr == '\0' || *start_itr == '\n') {
+  if (*start_itr == '\0' || *start_itr == '\n') {
+    data_pointers.push_back(nullptr);
     return data_pointers;
   }
 
-while(*start_itr != '\0'){
-  while (std::isgraph(*start_itr) == 0) {
-    start_itr++;
-  }
-  data_pointers.push_back(start_itr);
-  end_itr = start_itr;
+  while (*start_itr != '\0') {
+    while (std::isgraph(*start_itr) == 0) {
+      start_itr++;
+    }
+    data_pointers.push_back(start_itr);
+    end_itr = start_itr;
 
-  while (std::isgraph(*end_itr) != 0) {
-    end_itr++;
+    while (std::isgraph(*end_itr) != 0) {
+      end_itr++;
+    }
+    if (end_itr != '\0') {
+      *end_itr = '\0';
+      end_itr++;
+    }
+    start_itr = end_itr;
   }
-  if(end_itr != '\0') {
-    *end_itr = '\0';
-    end_itr++;
-  }
-  start_itr = end_itr;
-}
-return data_pointers;
+  data_pointers.push_back(nullptr);
+  return data_pointers;
 }
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,92 +1,96 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
+#include "pars_line.h"
 #include <array>
 #include <vector>
-#include <string.h>
 
-#include "pars_line.h"
-
-TEST_CASE("pars_line works properly", "[pars_line]")
-{
+TEST_CASE("pars_line works properly", "[pars_line]") {
 
   using Buff = std::array<char, 1000>;
   using Pointers = std::vector<char *>;
 
   Buff user_input;
   Buff null_str;
-  Pointers headers;
-  Pointers user_headers;
-  char prog_name[] = "ls";
-  char options_0[] = "-l";
-  char options_1[] = "-a";
+  Pointers out_start_lines;
+  Pointers in_start_lines;
 
-  SECTION("zero input")
-  {
+  SECTION("zero input") {
     user_input = Buff{"\0"};
-    headers = pars_line(user_input);
-    REQUIRE(headers.empty());
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines.size() == 1);
   }
 
-  SECTION("enter input")
-  {
+  SECTION("enter input а line feed only") {
     user_input = Buff{"\n"};
-    headers = pars_line(user_input);
-    REQUIRE(headers.empty());
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines.size() == 1);
   }
 
-  SECTION("command without arguments")
-  {
+  SECTION("command without arguments") {
     user_input = Buff{"ls"};
-    headers = pars_line(user_input);
-    REQUIRE(headers[0] == user_input.begin());
+    in_start_lines = {user_input.begin(), nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 2);
+    REQUIRE(user_input == Buff{"ls\0"});
   }
 
-  SECTION("command with argument")
-  {
+  SECTION("command with argument") {
     user_input = Buff{"ls -l"};
-    user_headers = {user_input.begin(), user_input.begin() + 3};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin(), user_input.begin() + 3, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 3);
+    REQUIRE(user_input == Buff{"ls\0-l\0"});
   }
 
-  SECTION("command with two arguments")
-  {
+  SECTION("command with two arguments") {
     user_input = Buff{"ls -l -a"};
-    user_headers = {user_input.begin(), user_input.begin() + 3, user_input.begin() + 6};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin(), user_input.begin() + 3,
+                      user_input.begin() + 6, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 4);
+    REQUIRE(user_input == Buff{"ls\0-l\0-a\0"});
   }
 
-  SECTION("command with two argument and two space delimeter")
-  {
+  SECTION("command with two argument and two space delimeter") {
     user_input = Buff{"ls  -l  -a"};
-    user_headers = {user_input.begin(), user_input.begin() + 4, user_input.begin() + 8};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin(), user_input.begin() + 4,
+                      user_input.begin() + 8, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 4);
+    REQUIRE(user_input == Buff{"ls\0 -l\0 -a\0"});
   }
 
-  SECTION("command with two argument and two space delimeter")
-  {
+  SECTION("command with two argument and two space delimeter") {
     user_input = Buff{"\tls\t-l\t-a\t"};
-    user_headers = {user_input.begin() + 1, user_input.begin() + 4, user_input.begin() + 7};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin() + 1, user_input.begin() + 4,
+                      user_input.begin() + 7, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 4);
+    REQUIRE(user_input == Buff{"\tls\0-l\0-a\0"});
   }
 
-  SECTION("command with two argument and two space delimeter")
-  {
+  SECTION("command with two argument and two space delimeter") {
     user_input = Buff{"  ls\t-l  -a\t"};
-    user_headers = {user_input.begin() + 2, user_input.begin() + 5, user_input.begin() + 9};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin() + 2, user_input.begin() + 5,
+                      user_input.begin() + 9, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 4);
+    REQUIRE(user_input == Buff{"  ls\0-l\0 -a\0"});
   }
 
-  SECTION("command with two argument and two space delimeter")
-  {
+  SECTION("command with two argument and two space delimeter") {
     user_input = Buff{"  ls\t\t\t-l  -a\t"};
-    user_headers = {user_input.begin() + 2, user_input.begin() + 7, user_input.begin() + 11};
-    headers = pars_line(user_input);
-    REQUIRE(headers == user_headers);
+    in_start_lines = {user_input.begin() + 2, user_input.begin() + 7,
+                      user_input.begin() + 11, nullptr};
+    out_start_lines = pars_line(user_input);
+    REQUIRE(out_start_lines == in_start_lines);
+    REQUIRE(out_start_lines.size() == 4);
+    REQUIRE(user_input == Buff{"  ls\0\t\t-l\0 -a\0"});
   }
 }
