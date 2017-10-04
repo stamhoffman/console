bf29dc4820a4b90a4eadc2301a30eefafe856a7c
stamhoffman <stamhoffman@yandex.ru>
2017-10-04 20:18:31 +0300

    Доработка интерфейса pars_line

--- src/server/parse_line.cpp
+++ src/server/parse_line.cpp
@@ -7,47 +7,33 @@ std::vector<char *> parse_line(std::array<char, 1000> &user_string) {
 
   std::vector<char *> data_pointers;
   std::array<char, 1000>::iterator start_itr = user_string.begin();
-  std::array<char, 1000>::iterator end_itr = user_string.end();
 
   while (start_itr != user_string.end()) {
     start_itr = std::find_if_not(start_itr, user_string.end(), isblank_wrapper);
     if (start_itr == user_string.end()) {
-      data_pointers.clear();
-      return data_pointers;
+      break;
     }
     if (*start_itr == '\0' && !data_pointers.empty()) {
       data_pointers.push_back(nullptr);
       return data_pointers;
     }
-
     if (!std::isgraph(*start_itr)) {
-      data_pointers.clear();
-      return data_pointers;
+      break;
     }
-
     data_pointers.push_back(start_itr);
-    end_itr = start_itr;
-
-    end_itr = std::find_if_not(start_itr, user_string.end(), isgraph_wrapper);
-    if (end_itr == user_string.end()) {
-      data_pointers.clear();
-      return data_pointers;
+    start_itr = std::find_if_not(start_itr, user_string.end(), isgraph_wrapper);
+    if (start_itr == user_string.end()) {
+      break;
     }
-
-    if (*end_itr == '\0') {
+    if (*start_itr == '\0') {
       data_pointers.push_back(nullptr);
       return data_pointers;
     }
-
-    if (!std::isblank(*end_itr)) {
-      data_pointers.clear();
-      return data_pointers;
+    if (!std::isblank(*start_itr)) {
+      break;
     }
-
-    start_itr = end_itr + 1;
-    if (start_itr != user_string.end()) {
-      *end_itr = '\0';
-    }
+    *start_itr = '\0';
+    start_itr++;
   }
   data_pointers.clear();
   return data_pointers;
