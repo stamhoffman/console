1ff4724c8194bad3533401338a58118304cf9f94
stamhoffman <stamhoffman@yandex.ru>
2017-10-06 23:04:06 +0300

    Возвращен тип char

--- src/server/parse_line.cpp
+++ src/server/parse_line.cpp
@@ -1,16 +1,16 @@
 #include "config.h"
 #include "parse_line.h"
 
-bool iswblank_wrapper(wchar_t symbol);
-bool iswgraph_wrapper(wchar_t symbol);
-void reset_vector(void);
-std::vector<wchar_t *> parse_line(std::array<wchar_t, 1000> &user_string) {
+bool isblank_wrapper(char symbol);
+bool isgraph_wrapper(char symbol);
 
-  std::vector<wchar_t *> data_pointers;
-  std::array<wchar_t, 1000>::iterator cur_itr = user_string.begin();
+std::vector<char *> parse_line(std::array<char, 1000> &user_string) {
+
+  std::vector<char *> data_pointers;
+  std::array<char, 1000>::iterator cur_itr = user_string.begin();
 
   while (cur_itr != user_string.end()) {
-    cur_itr = std::find_if_not(cur_itr, user_string.end(), iswblank_wrapper);
+    cur_itr = std::find_if_not(cur_itr, user_string.end(), isblank_wrapper);
     if (cur_itr == user_string.end()) {
       data_pointers.clear();
       break;
@@ -18,12 +18,12 @@ std::vector<wchar_t *> parse_line(std::array<wchar_t, 1000> &user_string) {
     if (*cur_itr == '\0' && !data_pointers.empty()) {
       break;
     }
-    if (!std::iswgraph(*cur_itr)) {
+    if (!std::isgraph(*cur_itr)) {
       data_pointers.clear();
       break;
     }
     data_pointers.push_back(cur_itr);
-    cur_itr = std::find_if_not(cur_itr, user_string.end(), iswgraph_wrapper);
+    cur_itr = std::find_if_not(cur_itr, user_string.end(), isgraph_wrapper);
     if (cur_itr == user_string.end()) {
       data_pointers.clear();
       break;
@@ -31,7 +31,7 @@ std::vector<wchar_t *> parse_line(std::array<wchar_t, 1000> &user_string) {
     if (*cur_itr == '\0') {
       break;
     }
-    if (!std::iswblank(*cur_itr)) {
+    if (!std::isblank(*cur_itr)) {
       data_pointers.clear();
       break;
     }
@@ -44,5 +44,5 @@ std::vector<wchar_t *> parse_line(std::array<wchar_t, 1000> &user_string) {
   return data_pointers;
 }
 
-bool iswblank_wrapper(wchar_t symbol) { return std::iswblank(symbol); }
-bool iswgraph_wrapper(wchar_t symbol) { return std::iswgraph(symbol); }
+bool isblank_wrapper(char symbol) { return std::isblank(symbol); }
+bool isgraph_wrapper(char symbol) { return std::isgraph(symbol); }
