84ada8deebf03937c3a80bf0943252d67495bf15
Ruzanov Vladimir <nnovzver@gmail.com>
2017-10-07 19:40:43 +0300

    Шаблонизация parse_line

--- src/server/parse_line.h
+++ src/server/parse_line.h
@@ -16,5 +16,54 @@
 // содержать строка, а также если в ней не содержится команда.
 // В случае ошибки функция возвращает вектор нулевого размера.
 // В случае ошибки буфер находится в неопределенном состоянии и должен игнорироваться.
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <cctype>
 
-std::vector<char*> parse_line(std::array<char, 1000> &user_str);
+namespace
+{
+bool isblank_wrapper(char symbol) { return std::isblank(symbol); }
+bool isgraph_wrapper(char symbol) { return std::isgraph(symbol); }
+}
+
+template <size_t N>
+std::vector<char *> parse_line(std::array<char, N> &user_string) {
+
+  std::vector<char *> data_pointers;
+  auto cur_itr = user_string.begin();
+
+  while (cur_itr != user_string.end()) {
+    cur_itr = std::find_if_not(cur_itr, user_string.end(), isblank_wrapper);
+    if (cur_itr == user_string.end()) {
+      data_pointers.clear();
+      break;
+    }
+    if (*cur_itr == '\0') {
+      break;
+    }
+    if (!std::isgraph(*cur_itr)) {
+      data_pointers.clear();
+      break;
+    }
+    data_pointers.push_back(cur_itr);
+    cur_itr = std::find_if_not(cur_itr, user_string.end(), isgraph_wrapper);
+    if (cur_itr == user_string.end()) {
+      data_pointers.clear();
+      break;
+    }
+    if (*cur_itr == '\0') {
+      break;
+    }
+    if (!std::isblank(*cur_itr)) {
+      data_pointers.clear();
+      break;
+    }
+    *cur_itr = '\0';
+    cur_itr++;
+  }
+  if (!data_pointers.empty()) {
+    data_pointers.push_back(nullptr);
+  }
+  return data_pointers;
+}
