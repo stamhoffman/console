ac9e0ba1236947add256eef2c5b47fb6768c2ea2
stamhoffman <stamhoffman@yandex.ru>
2017-12-18 00:46:04 +0300

    Исправление ошибок

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,7 +2,7 @@
 #include "parse_line.h"
 const int PORT = 54321;
 
-int ftp_connect(int &list_socket, struct sockaddr_in &serv_addr);
+int tcp_connect(int &list_socket, struct sockaddr_in &serv_addr);
 int execute_command(std::vector<char *> &data_pointers, int client_socket);
 int client_task(int client_socket);
 
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
   struct sockaddr_in client_addr;
   memset(&client_addr, 0, sizeof(client_addr));
 
-if (!ftp_connect(list_socket, serv_addr)){
-  std::cout << "Error ftp_connect:" << strerror(errno) << '\n';
+if (!tcp_connect(list_socket, serv_addr)){
+  std::cout << "Error tcp_connect:" << strerror(errno) << '\n';
   return -1;
 }
 
@@ -24,6 +24,7 @@ if (!ftp_connect(list_socket, serv_addr)){
     socklen_t length = sizeof(client_addr);
     std::cout << "start accept" << '\n';
     client_socket = accept(list_socket, (ps *)&client_addr, &length);
+
     if (client_socket == -1) {
       std::cout << "Errorr accept():" << strerror(errno) << "\n";
       close(list_socket);
@@ -34,24 +35,23 @@ if (!ftp_connect(list_socket, serv_addr)){
 
     inet_ntop(AF_INET, &(client_addr.sin_addr), addr_clients, INET_ADDRSTRLEN);
     std::cout << "connection from:" << addr_clients << "\n";
-
     int p_pid;
-    int status;
+
     p_pid = fork();
+
     if (p_pid == -1) {
       std::cout << "Error fork()" << strerror(errno) << std::endl;
       close(client_socket);
-      exit(1);
-    } else if (p_pid == 0) {
-      client_task(client_socket);
-    } else {
-      while (wait(&status) != p_pid)
-        ;
+      break;
     }
-  }
-
-  close(list_socket);
-  return 1;
+    if (p_pid == 0) {
+      client_task(client_socket);
+    }
+}
+int status;
+close(list_socket);
+wait(&status);
+return 1;
 }
 
 int client_task(int client_socket) {
@@ -59,18 +59,18 @@ int client_task(int client_socket) {
   std::cout << "client start(" << count << ")" << '\n';
 
   int read_byte = 0;
-
-  while (1) {
     read_byte = 0;
     std::array<char, 1000> user_str = {{'\0'}};
     std::vector<char *> data_pointers;
-    read_byte = read(client_socket, (void *)user_str.data(), user_str.size());
-    if (read_byte > 1) {
-      data_pointers = parse_line(user_str);
-      execute_command(data_pointers, client_socket);
+    while(read_byte > 0) {
+      read_byte = read(client_socket, (void *)user_str.data(), user_str.size());
     }
-  }
+    data_pointers = parse_line(user_str);
+    //execute_command(data_pointers, client_socket);
+    std::cout << "//execute_command(data_pointers, client_socket);" << std::endl;
+
   close(client_socket);
+  exit(1);
   return 0;
 }
 
@@ -94,11 +94,10 @@ int execute_command(std::vector<char *> &data_pointers, int client_socket) {
       return 0;
     }
   }
-
   return 1;
 }
 
-int ftp_connect(int &list_socket, struct sockaddr_in &serv_addr) {
+int tcp_connect(int &list_socket, struct sockaddr_in &serv_addr) {
   list_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (list_socket == -1) {
     std::cout << "Error socket():" << strerror(errno) << "\n";
@@ -111,7 +110,7 @@ int ftp_connect(int &list_socket, struct sockaddr_in &serv_addr) {
 
   const int on = 1;
   if (setsockopt(list_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) {
-    std::cout << "Error setsockopt():" << strerror(errno) << '\n';
+  std::cout << "Error setsockopt():" << strerror(errno) << '\n';
   }
 
   if ((bind(list_socket, (ps *)&serv_addr, sizeof(serv_addr))) == -1) {
